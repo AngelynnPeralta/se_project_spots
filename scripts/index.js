048888e66d4bc0b863daa814acff4c8b7d3944c5
Angelynn <angelynnsofte@gmail.com>
2025-06-10 15:34:08 -0700

    Add Sprint 4 Stage 3

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,32 @@
 const editProfileBtn = document.querySelector(".profile__edit-button");
 const editProfileModal = document.querySelector("#edit-profile-modal");
 const editProfileCloseBtn = editProfileModal.querySelector(".modal__close-btn");
+const editProfileNameInput = editProfileModal.querySelector(
+  "#profile-name-input"
+);
+const editProfileDescriptionInput = editProfileModal.querySelector(
+  "#profile-description-input"
+);
+const editProfileFormElement =
+  editProfileModal.querySelector("#edit-profile-form");
 
 const newPostBtn = document.querySelector(".profile__plus-button");
 const newPostModal = document.querySelector("#new-post-modal");
 const newPostCloseBtn = newPostModal.querySelector(".modal__close-btn");
+const newPostFormElement = newPostModal.querySelector("#new-post-form");
+const newPostCaptionInput = newPostModal.querySelector(
+  "#profile-caption-input"
+);
+const newPostImageLinkInput = newPostModal.querySelector("#card-image-input");
+
+const profileNameElement = document.querySelector(".profile__name");
+const profileDescriptionElement = document.querySelector(
+  ".profile__description"
+);
 
 editProfileBtn.addEventListener("click", function () {
+  editProfileNameInput.value = profileNameElement.textContent;
+  editProfileDescriptionInput.value = profileDescriptionElement.textContent;
   editProfileModal.classList.add("modal_is-opened");
 });
 
@@ -21,3 +41,23 @@ newPostBtn.addEventListener("click", function () {
 newPostCloseBtn.addEventListener("click", function () {
   newPostModal.classList.remove("modal_is-opened");
 });
+
+function handleProfileFormSubmit(evt) {
+  evt.preventDefault();
+  profileNameElement.textContent = editProfileNameInput.value;
+  profileDescriptionElement.textContent = editProfileDescriptionInput.value;
+  editProfileModal.classList.remove("modal_is-opened");
+}
+
+editProfileFormElement.addEventListener("submit", handleProfileFormSubmit);
+
+function handleNewPostSubmit(evt) {
+  evt.preventDefault();
+  newPostCaptionInput.value = newPostCaptionInput.textContent;
+  console.log(newPostCaptionInput);
+  newPostImageLinkInput.value = newPostImageLinkInput.textContent;
+  console.log(newPostImageLinkInput);
+  newPostModal.classList.remove("modal_is-opened");
+}
+
+newPostFormElement.addEventListener("submit", handleNewPostSubmit);
